9fe29be5244c1308610bde5ead19c13002e00af3
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2022-10-20 08:11:33 +0300

    Changed the way of Creators.

--- src/Components/Profile/Posts/Posts.jsx
+++ src/Components/Profile/Posts/Posts.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classes from './Posts.module.css';
 import Post from "./Post/Post";
-import {addNewPostTextCreator, updateNewPostTextCreator} from "../../../redux/state";
+import {addNewPostTextCreator, updateNewPostTextCreator} from "../../../redux/store";
 
 
 const Posts = (props) => {
