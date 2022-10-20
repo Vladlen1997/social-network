c27d64f2d2d5f67bdcb754f834bf5f08bedba5a7
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2022-10-20 08:19:05 +0300

    In the application component, I passed the dispatch (action) and data arrays.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import './App.css';
 import Header from "./Components/Header/Header";
 import Profile from "./Components/Profile/Profile";
 import NavBar from "./Components/NavBar/NavBar";
+import {Route, Routes} from "react-router-dom";
+import Messages from "./Components/Messages/Messages";
 
-const App = () => {
+const App = (props) => {
     return (
         <div className="app">
 
-            <Header/>
-            <NavBar/>
+            <Header header={props.state.header}/>
+            <NavBar navBar={props.state.navBar}/>
 
             <div className="main-wrapper">
-                <Profile/>
+                <Routes>
+                    <Route path='/Profile' element={<Profile profilePage={props.state.profilePage} dispatch={props.dispatch}/>}/>
+                    <Route path='/Messages' element={<Messages messagesPage={props.state.messagesPage} dispatch={props.dispatch}/>}/>
+                </Routes>
             </div>
         </div>
     )
