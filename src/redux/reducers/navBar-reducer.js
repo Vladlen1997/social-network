6aa13c17035f67e45dde829171196c769f8cc7f9
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2022-10-21 14:50:19 +0300

    added initialState to navBarReducer.

---
+++ src/redux/reducers/navBar-reducer.js
@@ -0,0 +1,18 @@
+let initialState = [
+    {page: 'Profile', id: 1},
+    {page: 'Messages', id: 2},
+    {page: 'Friends', id: 3},
+    {page: 'News', id: 4},
+    {page: 'Music', id: 5},
+    {page: 'Videos', id: 6}
+]
+
+
+const navBarReducer = (state = initialState, action) => {
+
+
+    return state;
+}
+
+
+export default navBarReducer;
