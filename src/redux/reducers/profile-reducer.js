3c736a130b89a9284986dcabdef1c01299d06bae
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2022-10-21 09:47:05 +0300

    moved the logic of adding a posts from dispatch.

---
+++ src/redux/reducers/profile-reducer.js
@@ -0,0 +1,26 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+export const addNewPostTextCreator = () => ({type: ADD_POST});
+export const updateNewPostTextCreator = (text) => ({type: UPDATE_NEW_POST_TEXT, text: text});
+
+const profileReducer = (state, action) => {
+
+    switch (action.type) {
+        case ADD_POST:
+            let newPost = {post: state.addNewPost, id: 4, likesCount: 8899};
+            state.post.push(newPost);
+            state.addNewPost = '';
+            return state;
+        case UPDATE_NEW_POST_TEXT:
+            state.addNewPost = action.text;
+            return state;
+        default:
+            return state;
+
+    }
+
+
+}
+
+
+export default profileReducer;
