d183a22cbe199a67d1970653ffc47eeb2ccf2f2c
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2022-10-21 14:48:54 +0300

    added initial state to headerReducer.

---
+++ src/redux/reducers/header-reducer.js
@@ -0,0 +1,15 @@
+let initialState = [
+    {reference: 'Settings', id: 1},
+    {reference: 'New', id: 2},
+    {reference: 'Log in', id: 3}
+]
+
+
+const headerReducer = (state = initialState, action) => {
+
+
+    return state;
+}
+
+
+export default headerReducer;
