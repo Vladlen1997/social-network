36994e604c4814b5ba66503d3c04fb1d05f27e0d
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2022-10-21 14:48:01 +0300

    added initial state to dialogsReducer.

--- src/redux/reducers/dialogs-reducer.js
+++ src/redux/reducers/dialogs-reducer.js
@@ -3,7 +3,22 @@ const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE_NEW_MESSAGE_TEXT';
 export const addNewMessageTextCreator = () => ({type: ADD_MESSAGE});
 export const updateNewMessageTextCreator = (message) => ({type: UPDATE_NEW_MESSAGE_TEXT, message: message});
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+        messages: [
+            {message: 'Hi, wow', id: 1},
+            {message: 'Hi, weh', id: 2},
+            {message: 'Hi, wuh', id: 3}
+        ],
+        interlocutor: [
+            {name: 'Oleg', id: 1},
+            {name: 'Andrey', id: 2},
+            {name: 'Stepa', id: 3}
+        ],
+        addNewMessage: ""
+    }
+
+
+const dialogsReducer = (state = initialState, action) => {
 
     switch (action.type) {
         case ADD_MESSAGE:
