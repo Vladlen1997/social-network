16cd2b78e2f6f9001ac1b4bc510b087518968bc5
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2022-10-21 14:50:52 +0300

    added initialState to profileReducer.

--- src/redux/reducers/profile-reducer.js
+++ src/redux/reducers/profile-reducer.js
@@ -3,7 +3,17 @@ const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 export const addNewPostTextCreator = () => ({type: ADD_POST});
 export const updateNewPostTextCreator = (text) => ({type: UPDATE_NEW_POST_TEXT, text: text});
 
-const profileReducer = (state, action) => {
+let initialState = {
+    post: [
+        {post: 'new apartment', id: 1, likesCount: 888},
+        {post: 'new architect', id: 2, likesCount: 788},
+        {post: 'news your friend', id: 3, likesCount: 988}
+    ],
+
+    addNewPost: ""
+
+}
+const profileReducer = (state = initialState, action) => {
 
     switch (action.type) {
         case ADD_POST:
