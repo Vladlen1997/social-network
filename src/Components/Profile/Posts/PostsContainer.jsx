089094943205d8ac5e01f424595c9916b373e6c2
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2022-10-25 19:37:47 +0300

    changed StoreContext to connect()();

--- src/Components/Profile/Posts/PostsContainer.jsx
+++ src/Components/Profile/Posts/PostsContainer.jsx
@@ -1,34 +1,29 @@
-import React from 'react';
 import {addNewPostTextCreator, updateNewPostTextCreator} from "../../../redux/reducers/profile-reducer";
 import Posts from "./Posts";
-import StoreContext from "../../../StoreContext";
+import {connect} from "react-redux";
 
-
-const PostsContainer = () => {
-
-    return (
-        <StoreContext.Consumer>
-            {
-                (store) => {
-
-                    let state = store.getState().profilePage;
-                    const sendNewPostText = () => {
-                        store.dispatch(addNewPostTextCreator());
-                    }
-
-                    const updateNewPostText = (text) => {
-                        store.dispatch(updateNewPostTextCreator(text));
-                    }
-                    return <Posts addNewPostText={sendNewPostText}
-                                  updateNewPostText={updateNewPostText}
-                                  profilePage={state}/>
-
-                }
-            }
-        < /StoreContext.Consumer>
-
-    )
+let mapStateToProps = (state) => {
+    return {
+        post: state.profilePage.post,
+        addNewPost: state.profilePage.addNewPost
+    }
 }
 
+let mapDispatchToProps = (dispatch) => {
+    return {
+        addNewPostText: () => {
+            dispatch(addNewPostTextCreator());
+        },
+
+        updateNewPostText: (text) => {
+            dispatch(updateNewPostTextCreator(text));
+        }
+    }
+}
+const PostsContainer = connect(mapStateToProps, mapDispatchToProps)(Posts);
 
 export default PostsContainer;
+
+
+
+
