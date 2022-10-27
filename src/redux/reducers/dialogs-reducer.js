ca774cce90ab8d1be64c31d13a7332ef1b4c8555
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2022-10-27 09:54:13 +0300

    refactoring dialogsReducer state copy

--- src/redux/reducers/dialogs-reducer.js
+++ src/redux/reducers/dialogs-reducer.js
@@ -4,35 +4,37 @@ export const addNewMessageTextCreator = () => ({type: ADD_MESSAGE});
 export const updateNewMessageTextCreator = (message) => ({type: UPDATE_NEW_MESSAGE_TEXT, message: message});
 
 let initialState = {
-        messages: [
-            {message: 'Hi, wow', id: 1},
-            {message: 'Hi, weh', id: 2},
-            {message: 'Hi, wuh', id: 3}
-        ],
-        interlocutor: [
-            {name: 'Oleg', id: 1},
-            {name: 'Andrey', id: 2},
-            {name: 'Stepa', id: 3}
-        ],
-        addNewMessage: ""
-    }
+    messages: [
+        {message: 'Hi, wow', id: 1},
+        {message: 'Hi, weh', id: 2},
+        {message: 'Hi, wuh', id: 3}
+    ],
+    interlocutor: [
+        {name: 'Oleg', id: 1},
+        {name: 'Andrey', id: 2},
+        {name: 'Stepa', id: 3}
+    ],
+    addNewMessage: ""
+}
 
 
 const dialogsReducer = (state = initialState, action) => {
-
     switch (action.type) {
         case ADD_MESSAGE: {
-            let newMessage = {message: state.addNewMessage, id: 4};
-            let stateCopy = {...state};
-            stateCopy.messages = [...state.messages];
-            stateCopy.messages.push(newMessage);
-            stateCopy.addNewMessage = '';
-            return stateCopy;
+            let body = state.addNewMessage;
+            return {
+                ...state,
+                addNewMessage: '',
+                messages: [...state.messages, {message: body, id: 4}]
+            };
+
         }
         case UPDATE_NEW_MESSAGE_TEXT: {
-            let stateCopy = {...state};
-            stateCopy.addNewMessage = action.message;
-            return stateCopy;
+            return {
+                ...state,
+                addNewMessage: action.message
+            };
+
         }
         default:
             return state;
