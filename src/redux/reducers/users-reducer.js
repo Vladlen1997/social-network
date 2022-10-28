df4a7b6c55e6635334362a92accf805b4bfde4a3
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2022-10-28 21:12:16 +0300

    Created an array with data and added logic to usersReducer

--- src/redux/reducers/users-reducer.js
+++ src/redux/reducers/users-reducer.js
@@ -1,27 +1,58 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
+const SET_USERS = 'SET-USERS';
+
 
 let initialState = {
     users: [
-        {id: 1, followed: false, fullName: 'Kolya', status: 'rules', location: {city: 'Minsk', country: 'Belarus'}},
+        /* {id: 1, followed: false, fullName: 'Kolya', status: 'rules', location: {city: 'Minsk', country: 'Belarus'}},
         {id: 2, followed: true, fullName: 'Victor', status: 'life', location: {city: 'Tbilisi', country: 'Georgia'}},
         {id: 3, followed: false, fullName: 'Lela', status: 'bulls', location: {city: 'Washington', country: 'USA'}},
         {id: 4, followed: true, fullName: 'Vladislav', status: 'pull', location: {city: 'Moscow', country: 'Russia'}},
         {id: 5, followed: false, fullName: 'Pavel', status: 'cool', location: {city: 'Kiev', country: 'Ukraine'}},
-    ]
+    */]
 };
 
 const usersReducer = (state = initialState, action) => {
 
     switch (action.type) {
+
+        case FOLLOW:
+            return {
+                ...state,
+                users: state.users.map(u => {
+                    if (u.id === action.userId) {
+                        return {...u, followed: true};
+                    }
+                    return u;
+                }),
+            }
+
+        case UNFOLLOW:
+            return {
+                ...state,
+                users: state.users.map(u => {
+                    if (u.id === action.userId) {
+                        return {...u, followed: false};
+                    }
+                    return u;
+                })
+            }
+
+        case SET_USERS: {
+            return {...state, users: [...state.users, ...action.users]};
+        }
         default:
             return state;
-    }
 
+
+    }
 }
 
-export const followActionCreator = () => ({type: FOLLOW});
-export const unfollowActionCreator = () => ({type: UNFOLLOW});
+
+export const followAC = (userId) => ({type: FOLLOW, userId});
+export const unfollowAC = (userId) => ({type: UNFOLLOW, userId});
+export const setUsersAC = (users) => ({type: SET_USERS, users});
 
 
 export default usersReducer;
