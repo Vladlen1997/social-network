e32b741a994eb0823fc35a9cf88ebdd82598581d
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2022-10-28 21:16:35 +0300

    added UsersReducer to redux-store

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,15 +3,15 @@ import profileReducer from "./reducers/profile-reducer";
 import dialogsReducer from "./reducers/dialogs-reducer";
 import headerReducer from "./reducers/header-reducer";
 import navBarReducer from "./reducers/navBar-reducer";
+import usersReducer from "./reducers/users-reducer";
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     messagesPage: dialogsReducer,
     header: headerReducer,
-    navBar: navBarReducer
+    navBar: navBarReducer,
+    usersPage: usersReducer
 });
 let store = createStore(reducers);
-
-
 
 export default store;
